b1fed5fc8d0002fafd30b890af7f0693f89ce399
lhollow75 <mathilde.deoliveira@gmail.com>
2017-01-11 06:49:12 +0100

    ajout de la configuration bdd localhost et prod

--- connexionbdd.php
+++ connexionbdd.php
@@ -1,8 +1,16 @@
 <?php
-define("SQL_HOST", "localhost");
-define("SQL_USER", "root");
-define("SQL_PASS", "");
-define("SQL_DBNAME", "isaac");
+if ($_SERVER["HTTP_HOST"]=='localhost'){
+	define("SQL_HOST", "localhost");
+	define("SQL_USER", "root");
+	define("SQL_PASS", "");
+	define("SQL_DBNAME", "isaac");
+} else if ($_SERVER["HTTP_HOST"]=='isaac-ia.com'){
+	define("SQL_HOST", "mysql.hostinger.fr");
+	define("SQL_USER", "u249459519_isaac");
+	define("SQL_PASS", "N7v>fmd^apB?3");
+	define("SQL_DBNAME", "u249459519_isaac");
+}
+
 try
 {
 	$mysql = new PDO("mysql:dbname=".SQL_DBNAME.";host=".SQL_HOST,SQL_USER,SQL_PASS);
