29f2d915dacef6623a6b5d565529d7ca0e1223cb
lhollow75 <mathilde.deoliveira@gmail.com>
2017-01-11 11:27:15 +0100

    les mails fonctionnent

--- envoimail.php
+++ envoimail.php
@@ -1,6 +1,7 @@
 <?php
+include('mail.php');
 if (!empty($_POST)){
-	var_dump($_POST);
+	// var_dump($_POST);
 	$mail=$_POST['form-mail']; // Déclaration de l'adresse de destination.
 	$sonmessage=$_POST['your-message'];
 }
@@ -15,7 +16,11 @@ else
 	$passage_ligne = "\n";
 }
 //=====Déclaration des messages au format texte et au format HTML.
-$message_txt = "Bonjour ISAAC,\nTu as reçu un nouveau mail de la part de :$mail\n\n$sonmessage";
+$message_txt = "Bonjour ISAAC,\nTu as un nouveau mail de la part de :$mail\n\n$sonmessage";
+$message_html = "<!doctype html>
+<html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:v=\"urn:schemas-microsoft-com:vml\" xmlns:o=\"urn:schemas-microsoft-com:office:office\">
+	Bonjour ISAAC,<br />Tu as un nouveau mail de la part de :$mail<br /><br />$sonmessage
+</html>";
 //==========
  
 //=====Création de la boundary
@@ -23,7 +28,7 @@ $boundary = "-----=".md5(rand());
 //==========
  
 //=====Définition du sujet.
-$sujet = "Nous avons reçu un nouveau message";
+$sujet = "Nous avons un nouveau message";
 //=========
  
 //=====Création du header de l'e-mail.
@@ -41,6 +46,11 @@ $message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
 $message.= $passage_ligne.$message_txt.$passage_ligne;
 //==========
 $message.= $passage_ligne."--".$boundary.$passage_ligne;
+//=====Ajout du message au format HTML
+$message.= "Content-Type: text/html; charset=\"ISO-8859-1\"".$passage_ligne;
+$message.= "Content-Transfer-Encoding: 8bit".$passage_ligne;
+$message.= $passage_ligne.$message_html.$passage_ligne;
+//==========
 $message.= $passage_ligne."--".$boundary."--".$passage_ligne;
 $message.= $passage_ligne."--".$boundary."--".$passage_ligne;
 //==========
@@ -48,5 +58,5 @@ $message.= $passage_ligne."--".$boundary."--".$passage_ligne;
 //=====Envoi de l'e-mail.
 mail($mailenvoi,$sujet,$message,$header);
 //==========
-//header('Location: ./'); 
+header('Location: ./'); 
 ?>
---
+++ mail.php
@@ -0,0 +1,5 @@
+<?php
+$message_html = "<!doctype html>
+<html xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:v=\"urn:schemas-microsoft-com:vml\" xmlns:o=\"urn:schemas-microsoft-com:office:office\">
+	Vous avez un nouveau message
+</html>";
