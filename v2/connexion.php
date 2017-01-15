df313a2826521b0d9d9c426d7e9ddfc78451a1cb
mrvink <vinkdesign@gmail.com>
2017-01-15 01:33:29 +0100

    Update connexion.php

--- v2/connexion.php
+++ v2/connexion.php
@@ -1,27 +1,192 @@
-<div class="wrapper" id="main-content">
-	<div class="container">
-		<div class="row">
-			<div class="col-lg-12">
-				<h1 class="page_titre">CONNEXION</h1>
-				<hr>
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>ISAAC - Intelligence artificielle</title>
+        <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/animate.css">
+        
+        <!--Meta -->
+        <meta name="viewport" content="initial-scale=1">
+        
+        
+        <!--Meta Twitter Card-->
+        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content="ISAAC Intelligence artificielle">
+        <meta name="twitter:description" content="Je suis ISAAC et je vous accompagne dans la création de votre boîte ! :)">
+        <meta name="twitter:site" content="@isaac__ia">
+        <meta name="twitter:creator" content="@isaac__ia">
+        <meta name="twitter:image" content="http://www.isaac-ia.com/img/isaac-logo.jpg">
+
+        <!--Meta Open Graph-->
+        <meta property="og:title" content="ISAAC Intelligence artificielle" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="http://www.isaac-ia.com/" />
+        <meta property="og:image" content="http://www.isaac-ia.com/img/isaac-logo.jpg" />
+        <meta property="og:description" content="Je suis ISAAC et je vous accompagne dans la création de votre boîte ! :)" />
+        <meta property="og:site_name" content="ISAAC" />
+        
+        <!--Fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+        
+        <!--Favicon-->
+        <link rel="apple-touch-icon-precomposed" sizes="57x57" href="http://www.isaac-ia.com/img/favicon/apple-touch-icon-57x57.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="http://www.isaac-ia.com/img/favicon/apple-touch-icon-114x114.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="http://www.isaac-ia.com/img/favicon/apple-touch-icon-72x72.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="http://www.isaac-ia.com/img/favicon/apple-touch-icon-144x144.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="60x60" href="http://www.isaac-ia.com/img/favicon/apple-touch-icon-60x60.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="120x120" href="http://www.isaac-ia.com/img/favicon/apple-touch-icon-120x120.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="76x76" href="http://www.isaac-ia.com/img/favicon/apple-touch-icon-76x76.png" />
+        <link rel="apple-touch-icon-precomposed" sizes="152x152" href="http://www.isaac-ia.com/img/favicon/apple-touch-icon-152x152.png" />
+        <link rel="icon" type="image/png" href="http://www.isaac-ia.com/img/favicon/favicon-196x196.png" sizes="196x196" />
+        <link rel="icon" type="image/png" href="http://www.isaac-ia.com/img/favicon/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/png" href="http://www.isaac-ia.com/img/favicon/favicon-32x32.png" sizes="32x32" />
+        <link rel="icon" type="image/png" href="http://www.isaac-ia.com/img/favicon/favicon-16x16.png" sizes="16x16" />
+        <link rel="icon" type="image/png" href="http://www.isaac-ia.com/img/favicon/favicon-128.png" sizes="128x128" />
+        <meta name="application-name" content="Isaac"/>
+        <meta name="msapplication-TileColor" content="#FFFFFF" />
+        <meta name="msapplication-TileImage" content="http://www.isaac-ia.com/img/favicon/mstile-144x144.png" />
+        <meta name="msapplication-square70x70logo" content="http://www.isaac-ia.com/img/favicon/mstile-70x70.png" />
+        <meta name="msapplication-square150x150logo" content="http://www.isaac-ia.com/img/favicon/mstile-150x150.png" />
+        <meta name="msapplication-wide310x150logo" content="http://www.isaac-ia.com/img/favicon/mstile-310x150.png" />
+        <meta name="msapplication-square310x310logo" content="http://www.isaac-ia.com/img/favicon/mstile-310x310.png" />
+
+        <!--Optimizely-->
+        <script src="https://cdn.optimizely.com/js/8152643656.js"></script>
+
+        <!-- Hotjar Tracking Code for www.isaac-ia.com -->
+        <script>
+            (function(h,o,t,j,a,r){
+                h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+                h._hjSettings={hjid:380826,hjsv:5};
+                a=o.getElementsByTagName('head')[0];
+                r=o.createElement('script');r.async=1;
+                r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+                a.appendChild(r);
+            })(window,document,'//static.hotjar.com/c/hotjar-','.js?sv=');
+        </script>
+        
+        <!--Google Analytics-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+	</head>
+    <body>
+        <!--<div class="lp-connexion">-->
+            
+            <!--Section gauche-->
+            <section class="connexion sectionLeft">
+                <!--<div class="slider"></div>-->
+                    
+                    
+
+	
+	<!-- ==================================== 
+	Contenedor Slider 
+	=======================================-->
+	<section id="slider" class="container">
+		<ul class="slider-wrapper">
+		<li class="current-slide">
+			<img src="../img/connexion/slide1.jpg" title="" alt="">
+
+			<div class="caption">
+				<p>&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Labore, placeat est. Alias illo hic quo nobis, aspernatur iste ut voluptate.&rdquo;</p>
+				<h2 class="slider-title">Grégory N. - Le poing levé</h2>
+
 			</div>
-			<form action="../formConnexion.php" method="post" id="connect-form" class="row">
-				<div class=" col-lg-6 col-md-6 col-sm-12 col-xs-12">
-					<input type="text" name="identifiant" value="" placeholder="Mail" required>
-					<p><a href="index.php?page=logon">Pas encore inscrit ?</a></p>
-				</div>
-				<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-					<input type="password" name="password" value="" placeholder="Mot de passe.." required>
-				</div>
-				<div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-					<input class="btn" type="submit" name="sumbit" value="Se connecter">
-					<?php 
-					if(isset($_GET['erreur'])) {
-						echo "<p>".htmlspecialchars($_GET['erreur'])."</p>";
-					}
-				 	?>
-				</div>
-			</form>
-		</div>
-	</div>
-</div>
+		</li>
+
+		<li>
+			<img src="../img/connexion/slide2.jpg" title="" alt="">
+
+			<div class="caption">
+				<p>"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo iusto placeat aliquid tempore harum, similique !"</p>
+				<h2 class="slider-title">John Edwin Graf - The Wild Mix</h2>
+
+			</div>
+		</li>
+
+		</ul>
+		<!-- Sombras -->
+		<div class="slider-shadow"></div>
+		
+		<!-- Controles de Navegacion -->
+		<ul id="control-buttons" class="control-buttons"></ul>
+	</section>
+	
+
+	</p>
+                    
+                    
+            
+                
+                
+            </section>
+            
+            <!--Section droite-->
+            <section class="connexion sectionRight">
+                <ul class="menu">
+                    <li><a href="#">Inscription</a></li>
+                    <li class="selected"><a href="#">Connexion</a></li>
+                    <li><a href="#">Quitter</a></li>
+                </ul>
+                
+                
+                
+                <div class="formConnexion">
+                    <form action="../formConnexion.php" id="connect-form" method="post">
+                        <h2>Connexion</h2>
+                        <ul class=forms>
+                            <li>
+                                <div class="picto-mail"></div>
+                                <input type="email" name="identifiant" id="form-mail-connexion" placeholder="Adresse e-mail" required/>
+                            </li>
+                        
+                            <li>
+                                <div class="picto-password"></div>
+                                <input type="password" name="password" id="form-password" placeholder="Mot de passe" required/>
+                            </li>
+                            
+                            <li>
+                                <p class="mdp">
+                                    <span><em>
+                                        <a href="#">Mot de passe oublié ?</a>
+                                        </em></span>
+                                </p>
+                            </li>
+                        </ul>
+                    
+                    </form>
+
+                    <button class="connexionButton" type="submit" name="sumbit">
+                        <p>Connexion</p>
+                        <div class="picto-arrow"></div>
+                    </button>
+                </div>
+                
+            </section>
+        
+        
+
+    <script src="dist/slider.js"></script>
+
+        <!--Analytics Google-->
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-89779336-1', 'auto');
+          ga('send', 'pageview');
+
+        </script>
+        
+        
+
+        
+        </script>
+    </body>
+
+    <!-- Wow -->
+    <script src="dist/wow.min.js"></script>
+    <script>new WOW().init();</script>
+</html>
